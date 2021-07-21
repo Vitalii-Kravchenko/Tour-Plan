8a5b1cbb200a39dcf4a1328b56686155aaebf5cd
vitalii-kravchenko <vitaliy.kravchenko.work@gmail.com>
2021-07-21 21:45:56 +0300

    Fix: scrolling in burger modal in js file

--- js/main.js
+++ js/main.js
@@ -31,7 +31,13 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 });
 
 let menuButton = document.querySelector(".burger-menu");
+let body = document.querySelector("body");
 
 menuButton.addEventListener("click", function() {
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--mobile_visible");
+  if(body.style.overflow == "hidden") {
+    body.style.overflow = "auto";
+  } else {
+    body.style.overflow = "hidden"
+  }
 });
